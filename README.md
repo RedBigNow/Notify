4b091d106fb365c2711aa109d36671a7a94b080e
Nikita <nikita_sokolov98@mail.ru>
2021-09-02 19:56:58 +0300

    app started

---
+++ README.md
@@ -0,0 +1,22 @@
+# Notify (Vue.js 2)
+
+## Demo:
+https://redbignow.github.io/Notify/dist/index.html
+
+## Project setup
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+```
+npm run serve
+```
+
+### Compiles and minifies for production
+```
+npm run build
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
---
+++ src/pages/NotifyPage.vue
@@ -0,0 +1,71 @@
+<template>
+    <div class="wrapper-content wrapper-content--fixed">
+        <section>
+            <div class="container">
+                <div class="notify__wrapper">
+
+                    <div class="notify__title">
+                        <p>Notify App</p>
+                    </div>
+
+                    <div class="notify__content">
+                        <notify
+                            :messages="messages"
+                        />
+                    </div>
+
+                </div>
+            </div>
+        </section>
+    </div>
+</template>
+
+<script>
+import notify from '@/components/Notify.vue'
+
+export default {
+    components: { notify },
+    data () {
+        return {
+            messages: [
+                { title: 'message 1' },
+                { title: 'message 2' },
+                { title: 'message 3' },
+                { title: 'message 4' },
+                { title: 'message 5' },
+                { title: 'message 6' }
+            ]
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+.container {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    height: 90vh;
+}
+
+.notify__wrapper {
+    width: 400px;
+    background-color: #fff;
+    padding: 30px;
+    border-radius: 16px;
+    box-shadow: 0 12px 22px 0 rgba(0,0,0,.1);
+}
+
+.notify__title {
+    p {
+        font-size: 24px;
+    }
+}
+
+.notify__content {
+    display: flex;
+    align-items: center;
+    flex-direction: column;
+    min-height: 300px;
+}
+</style>
